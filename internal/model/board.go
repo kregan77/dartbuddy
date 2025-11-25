2288f5f9e98b0d1553545b42fb7c5a7bebf1fa5c
Kevin Regan <kregan77@hotmail.com>
2025-11-24 20:35:05 -0500

    getting started

---
+++ internal/model/board.go
@@ -0,0 +1,32 @@
+package model
+
+const (
+	Bullseye  = 25
+	Twenty    = 20
+	Nineteen  = 19
+	Eighteen  = 18
+	Seventeen = 17
+	Sixteen   = 16
+	Fifteen   = 15
+	Fourteen  = 14
+	Thirteen  = 13
+	Twelve    = 12
+	Eleven    = 11
+	Ten       = 10
+	Nine      = 9
+	Eight     = 8
+	Seven     = 7
+	Six       = 6
+	Five      = 5
+	Four      = 4
+	Three     = 3
+	Two       = 2
+	One       = 1
+)
+
+var Board = []int{
+	Twenty, One, Eighteen, Four, Thirteen,
+	Six, Ten, Fifteen, Two, Seventeen,
+	Three, Nineteen, Seven, Sixteen, Eight,
+	Eleven, Fourteen, Nine, Twelve, Five,
+}
